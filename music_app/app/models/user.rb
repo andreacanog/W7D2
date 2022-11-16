528d7e23b5612b699bacce7dce369606153b3293
Andrea Cano <andreacanogisbert@gmail.com>
2022-11-15 17:38:00 -0800

    controllers done

--- music_app/app/models/user.rb
+++ music_app/app/models/user.rb
@@ -12,26 +12,49 @@
 class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true
     validates :password_digest, presence: true
-    validates :session_token, :presence: true, uniqueness: true 
+    validates :session_token, presence: true, uniqueness: true 
     validates :password, length: { minimum: 6 }, allow_nil: true 
 
-    def find_by_credentials(email, password)
+    attr_reader :password
+
+    before_validation :ensure_session_token
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        if user && user.is_password?(password)
+            user
+        else
+            nil 
+        end
     end
 
     def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
     end
 
     def reset_session_token!
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token
     end
 
     def ensure_session_token
+        self.session_token ||= generate_unique_session_token
     end
 
     def is_password?(password)
+        bcrypt_object = BCrypt::Password.new(self.password_digest)
+        bcrypt_object.is_password?(password)
     end
 
     private 
 
     def generate_unique_session_token
+        token = SecureRandom::urlsafe_base64
+        while User.exists?(session_token: token)
+            token = SecureRandom::urlsafe_base64
+        end
+        token
     end
 end
